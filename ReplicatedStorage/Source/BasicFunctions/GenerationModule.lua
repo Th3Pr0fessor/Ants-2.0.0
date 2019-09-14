edffbcfc0a660ffcd8d10cc8e3e05b9e665fabbc
Th3Pr0fessor <47043384+Th3Pr0fessor@users.noreply.github.com>
2019-09-13 23:29:49 -0400

    Create GenerationModule.lua

---
+++ ReplicatedStorage/Source/BasicFunctions/GenerationModule.lua
@@ -0,0 +1,15 @@
+local Ants = {}
+
+Ants.Generate = function(Part, wt, Resource)
+    while wait(wt) do
+        local PC = Part:Clone()
+        PC.Size = Vector3.new(4,4,4)
+        PC.Name = Resource
+        PC.Parent = game.Workspace
+        PC.Position = Part.Position + Vector3.new(math.random(-100,100),0,math.random(-100,100))
+        PC.Script:Destroy()
+        PC.Transparency = 0
+    end
+end
+
+return Ants
